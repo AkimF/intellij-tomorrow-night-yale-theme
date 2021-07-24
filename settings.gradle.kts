8eb9febc1f3adcbdeb6cd6fb7004562aa63eb4c5
GitHub Action <action@github.com>
2021-07-24 06:12:05 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "tomorrow-night-yale-theme"
---
+++ src/main/kotlin/com/github/rudilee/tomorrownightyaletheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rudilee.tomorrownightyaletheme.services
+
+import com.github.rudilee.tomorrownightyaletheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
